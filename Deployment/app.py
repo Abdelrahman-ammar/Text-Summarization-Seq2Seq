06bb7ad1ff85b5499c12ce8f5f8f3d223cb135e1
Abdelrahman Yasser <112268441+Abdelrahman-ammar@users.noreply.github.com>
2024-09-29 17:06:38 +0300

    first commit

---
+++ Deployment/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ced357aee2df979c6b43346f30140ba4143bd9fc68a086e4417afa00d98e912
+size 1704
---
+++ Deployment/utils.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:154b270e2f2cf35c07f188b600e39bc76514e91e90d372c4e29e5e0ef91f6b06
+size 3228
